26cd4085c86ac76abda3e7118f4cab6ecaa29b3b
muukvds <muukvds@users.noreply.github.com>
2017-09-21 11:36:28 +0200

    fix nullpointexeption in CD class

--- src/CD.java
+++ src/CD.java
@@ -11,53 +11,62 @@ public class CD {
         return title;
     }
 
-    public void addSong(Song song)
-    {
+    public void addSong(Song song) {
         boolean songAdded = false;
-            for (int i = 0; i < songs.length; i++) {
-                if (!songAdded) {
-                    if (songs[i] == null) {
-                        songs[i] = song;
-                        songAdded = true;
-                    }
+        for (int i = 0; i < songs.length; i++) {
+            if (!songAdded) {
+                if (songs[i] == null) {
+                    songs[i] = song;
+                    songAdded = true;
                 }
             }
+        }
         if (!songAdded) {
             System.out.println("CD is vol");
         }
     }
 
-    public int getLength()
-    {
-        int totallength=0;
-        for (Song song:songs) { totallength += song.getLength(); }
+    public int getLength() {
+        int totallength = 0;
+        for (Song song : songs) {
+            if (song != null) {
+                totallength += song.getLength();
+            }
+        }
         return totallength;
     }
 
-    public void printLongestTrack()
-    {
-        Song longgestSong= songs[0];
-        int track=0;
-        for (int i=0;i< songs.length; i++
-             ) {
-            if(songs[i].getLength() > longgestSong.getLength()){longgestSong = songs[i]; track=i+1;}
-
+    public void printLongestTrack() {
+        Song longestSong = songs[0];
+        int track = 0;
+        for (int i = 0; i < songs.length; i++
+                ) {
+            if (songs[i] != null) {
+                if (songs[i].getLength() > longestSong.getLength()) {
+                    longestSong = songs[i];
+                    track = i + 1;
+                }
+            }
         }
 
-        System.out.println("*** Longest Track on CD: "+title+" ***");
-        printTrack(track,longgestSong);
+        System.out.println("*** Longest Track on CD: " + title + " ***");
+        printTrack(track, longestSong);
     }
 
-    public void printInfo()
-    {
-        for(int i=0 ;i < songs.length;i++)
-        {
-            printTrack(i+1, songs[i]);
+    public void printInfo() {
+
+
+        System.out.println("*** CD Information ***");
+        System.out.println("CD: "+title+", Total Length: "+getLength()+" sec.");
+
+        for (int i = 0; i < songs.length; i++) {
+            if (songs[i] != null) {
+                printTrack(i + 1, songs[i]);
+            }
         }
     }
 
-    private void printTrack(int track, Song song)
-    {
-        System.out.println("Track "+track+": "+song.getTitle()+", "+song.getArtist()+", "+song.getLength()+" sec.");
+    private void printTrack(int track, Song song) {
+        System.out.println("Track " + track + ": " + song.getTitle() + ", " + song.getArtist() + ", " + song.getLength() + " sec.");
     }
 }

6db8830463f03a388ecad123dbc179f0eadd0db9
muukvds <muukvds@users.noreply.github.com>
2017-09-21 10:53:07 +0200

    create project, add Song class

---
+++ src/Song.java
@@ -0,0 +1,21 @@
+public class Song {
+    private String title;
+    private String artist;
+    private int length;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}

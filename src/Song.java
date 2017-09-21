14f8ff535d7e446e6cd3708982edddd047b01e7e
muukvds <muukvds@users.noreply.github.com>
2017-09-21 11:17:28 +0200

    add getters

--- src/Song.java
+++ src/Song.java
@@ -3,6 +3,14 @@ public class Song {
     private String artist;
     private int length;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
     public int getLength() {
         return length;
     }
